60c1ad4f726b33fc4c00a075986275e53717b648
Daino360 <33093848+Daino360@users.noreply.github.com>
2025-06-03 15:20:37 +0200

    First try
    Base code for Book Of Love

---
+++ BookOfLove/firebase-config.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBh7R_Ms7ilYvdHIzM0dRWwJJ09Q-EAXB0",
+    authDomain: "bookoflove-d6dd1.firebaseapp.com",
+    projectId: "bookoflove-d6dd1",
+    storageBucket: "bookoflove-d6dd1.firebasestorage.app",
+    messagingSenderId: "293815616308",
+    appId: "1:293815616308:web:8e750943b35babf65a1b61",
+    measurementId: "G-Z23J72080V"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
