444d613f9d7ab1142212ea6dfc70a2273e119772
Daino360 <33093848+Daino360@users.noreply.github.com>
2025-06-03 17:13:04 +0200

    Aggiustamento config

--- firebase-config.js
+++ firebase-config.js
@@ -1,30 +1,20 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyBh7R_Ms7ilYvdHIzM0dRWwJJ09Q-EAXB0",
+  authDomain: "bookoflove-d6dd1.firebaseapp.com",
+  databaseURL: "https://bookoflove-d6dd1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bookoflove-d6dd1",
+  storageBucket: "bookoflove-d6dd1.firebasestorage.app",
+  messagingSenderId: "293815616308",
+  appId: "1:293815616308:web:8e750943b35babf65a1b61",
+  measurementId: "G-Z23J72080V"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBh7R_Ms7ilYvdHIzM0dRWwJJ09Q-EAXB0",
-    authDomain: "bookoflove-d6dd1.firebaseapp.com",
-    databaseURL: "https://bookoflove-d6dd1-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bookoflove-d6dd1",
-    storageBucket: "bookoflove-d6dd1.firebasestorage.app",
-    messagingSenderId: "293815616308",
-    appId: "1:293815616308:web:8e750943b35babf65a1b61",
-    measurementId: "G-Z23J72080V"
-  };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const auth = getAuth(app);
-  
-  export { db, auth };
-
-</script>
+export { db, auth };
