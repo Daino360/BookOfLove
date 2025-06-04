4414d2cc0341d15c429a714a5155898d77d816c2
Daino360 <33093848+Daino360@users.noreply.github.com>
2025-06-04 11:46:07 +0200

    Updated with v8 of firebase

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Replace with your Firebase config
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBh7R_Ms7ilYvdHIzM0dRWwJJ09Q-EAXB0",
-    authDomain: "bookoflove-d6dd1.firebaseapp.com",
-    databaseURL: "https://bookoflove-d6dd1-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bookoflove-d6dd1",
-    storageBucket: "bookoflove-d6dd1.firebasestorage.app",
-    messagingSenderId: "293815616308",
-    appId: "1:293815616308:web:8e750943b35babf65a1b61",
-    measurementId: "G-Z23J72080V"
-  };
+  apiKey: "AIzaSyBh7R_Ms7ilYvdHIzM0dRWwJJ09Q-EAXB0",
+  authDomain: "bookoflove-d6dd1.firebaseapp.com",
+  databaseURL: "https://bookoflove-d6dd1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bookoflove-d6dd1",
+  storageBucket: "bookoflove-d6dd1.firebasestorage.app",
+  messagingSenderId: "293815616308",
+  appId: "1:293815616308:web:8e750943b35babf65a1b61",
+  measurementId: "G-Z23J72080V"
+};
 
 // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const memoriesCollection = db.collection("memories");
+
 // DOM Elements
 const memoryForm = document.getElementById('memoryForm');
 const memoryDisplay = document.getElementById('memoryDisplay');
